2f3fbad122f852fd5d8d508dcc243447623253e8
Тимирзин Егор <timirzin.egor@yandex.ru>
2022-09-17 15:23:06 +0400

    Добавьте файлы проекта.

---
+++ Hackathon2022/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hackathon2022.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Hackathon2022/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// добавляем в приложение сервисы Razor Pages
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// добавляем поддержку маршрутизации для Razor Pages
+app.MapRazorPages();
+app.UseStaticFiles();
+
+app.Run();
